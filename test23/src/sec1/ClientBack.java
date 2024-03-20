4cb736b741fd868163a53d0090f04d8e29da00b5
jwj4546 <jwj4546@naver.com>
2024-03-20 11:33:35 +0900

    java11 TCP & UDP

--- test23/src/sec1/ClientBack.java
+++ test23/src/sec1/ClientBack.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.net.Socket;
 import java.util.ArrayList;
 
+import sec1.ServerBack.ReceiveInfo;
+
 public class ClientBack extends Thread {
 	private String NickName, IPAddress;
 	private int Port;
